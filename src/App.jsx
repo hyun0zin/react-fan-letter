2fcb1d29ed8bcd78e069bec389d4d189151f9a96
HyunjinKim <koujie11@gmail.com>
2024-01-30 23:40:43 +0900

    feat : component 분리

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ import LetterList from "components/LetterList";
 import Letter from "components/Letter";
 import Footer from "components/Footer";
 
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
-  margin: 20px;
-`;
+// const StBox = styled.div`
+//   width: 100px;
+//   height: 100px;
+//   border: 1px solid black;
+//   margin: 20px;
+// `;
 
 function App() {
   return (
     <>
-      <StBox>App</StBox>
+      {/* <StBox>App</StBox> */}
       <Header />
       <MemberSelect />
       <WriteLetter />
