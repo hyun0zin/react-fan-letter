0670a1d8df81a1616848fab798a1d05f3c328f02
HyunjinKim <koujie11@gmail.com>
2024-01-30 21:51:20 +0900

    feat : header css 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const StHead = styled.header`
+  background-color: black;
+  color: white;
+  height: 5rem;
+
+  text-align: center;
+`;
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <StHead>BLACKPINK</StHead>
+    </>
+  );
 }
 
 export default Header;
