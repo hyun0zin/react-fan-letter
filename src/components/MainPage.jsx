10a691dcb07cc1711536436dbea12d9d810cb892
HyunjinKim <koujie11@gmail.com>
2024-01-31 15:44:15 +0900

    feat : 홈화면 mainpage로 만들기

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MemberSelect from "./MemberSelect";
+import WriteLetter from "./WriteLetter";
+
+function MainPage() {
+  return (
+    <div>
+      <MemberSelect />
+      <WriteLetter />
+    </div>
+  );
+}
+
+export default MainPage;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,18 +3,21 @@ import JENNIE from "pages/JENNIE";
 import JISOO from "pages/JISOO";
 import ROSE from "pages/ROSE";
 import LISA from "pages/LISA";
-import Layout from "components/Layout";
+import Layout from "components/layout/Layout";
+import MainPage from "components/MainPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/jennie" element={<JENNIE />} />
-        <Route path="/jisoo" element={<JISOO />} />
-        <Route path="/rose" element={<ROSE />} />
-        <Route path="/lisa" element={<LISA />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/jennie" element={<JENNIE />} />
+          <Route path="/jisoo" element={<JISOO />} />
+          <Route path="/rose" element={<ROSE />} />
+          <Route path="/lisa" element={<LISA />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
