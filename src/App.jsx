cd403767108504aecf7e5ebc524a4e2242e72372
HyunjinKim <koujie11@gmail.com>
2024-01-31 19:13:34 +0900

    fix : footer img link 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import GlobalStyle from "GlobalStyle";
 import Router from "shared/Router";
-import Layout from "components/layout/Layout";
-import LetterList from "components/letters/LetterList";
-import Letter from "components/letters/LetterItem";
 
 function App() {
   return (
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import youtubeLogo from "../../assets/images/youtubeLogo.png";
 
 const StFooter = styled.footer`
   background-color: black;
   color: white;
   height: 5rem;
 
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
   /* position: sticky;
   bottom: 0; */
 `;
+const StImgLink = styled.a`
+  display: block;
+`;
+const StImgYoutube = styled.img.attrs({
+  src: youtubeLogo,
+  alt: "유튜브 로고",
+})`
+  height: 25px;
+  cursor: pointer;
+`;
 function Footer() {
-  return <StFooter>footer 부분입니다</StFooter>;
+  return (
+    <>
+      <StFooter>
+        <StImgLink href="http://www.youtube.com/@BLACKPINK">
+          <StImgYoutube />
+        </StImgLink>
+      </StFooter>
+    </>
+  );
 }
 
 export default Footer;
